bd42d8a7553f26a8bba57fa29d73bb8ce1034dc5
Dane Petersen <thegreatsunra@gmail.com>
2018-03-06 15:03:07 -0600

    Fix header assignment again

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ proxy.listen(config.proxy.port)
 
 proxy.on('proxyRes', (proxyRes, req, res) => {
   console.log('Sent response from proxy')
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 })
 
 proxy.on('error', (error) => {
