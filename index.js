b8bbc76ebc44c06547a059ef4d1bf7479c0b5f75
Dane Petersen <thegreatsunra@gmail.com>
2018-03-07 11:38:52 -0600

    Add generous CORS headers to responses

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express()
 
 app.use(helmet())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
 
 app.use(express.static('public'))
