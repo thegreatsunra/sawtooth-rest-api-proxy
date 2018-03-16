170783b3161d2cc9bcc42df126d678aa0bc52903
Dane Petersen <thegreatsunra@gmail.com>
2018-03-16 14:57:04 -0500

    Generate a new basic-auth username and password when you run init.js

--- init.js
+++ init.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const fs = require('fs')
 const helmet = require('helmet')
+const path = require('path')
 
 const env = require('./env')
+const random = require('./random')
 
 const app = express()
+
+const username = random.createWord() + '-' + random.createWord()
+const password = random.createString(24, 'lowercasenumbers')
+
+const output = `USERNAME=${username}\nPASSWORD=${password}\nUSE_HTTPS=false`
+
+fs.writeFile(path.resolve(__dirname, './.env'), output, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+})
+
+console.log('Saved a new basic-auth username and password combination to .env\n')
+console.log(`USERNAME:\n${username}\n`)
+console.log(`PASSWORD:\n${password}\n`)
+
 
 app.use(helmet())
 
