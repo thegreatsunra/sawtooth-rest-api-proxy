11883d1b43ffb307438d9fa8a1a826a958826420
Dane Petersen <thegreatsunra@gmail.com>
2018-03-16 14:54:55 -0500

    Consolidate config and env, add ability to override defaults with dotenv, split http and https logging

--- env.js
+++ env.js
@@ -7,6 +7,27 @@ const env = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   useHttps: yn(process.env.USE_HTTPS) || false,
+  proxy: {
+    initPort: process.env.PROXY_INIT_PORT || 80,
+    internalPort: process.env.PROXY_INTERNAL_PORT || 8887,
+    publicPort: process.env.PROXY_PUBLIC_PORT || 8888,
+    sslCert: process.env.PROXY_SSL_CERT_PATH || './sslcert/fullchain.pem',
+    sslKey: process.env.PROXY_SSL_KEY_PATH || './sslcert/privkey.pem'
+  },
+  sawtoothRestApi: {
+    host: process.env.SAWTOOTH_REST_API_HOST || 'localhost',
+    port: process.env.SAWTOOTH_REST_API_PORT || 8008,
+    endpoints: [
+      'batches',
+      'batch_statuses',
+      'blocks',
+      'peers',
+      'receipts',
+      'state',
+      'status',
+      'transactions'
+    ]
+  }
 }
 
 module.exports = env
--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const https = require('https')
 const morgan = require('morgan')
 const path = require('path')
 
-const config = require('./config')
 const env = require('./env')
 
 const app = express()
 
+const sawtoothRestApiUrl = `${ env.sawtoothRestApi.host }:${ env.sawtoothRestApi.port }`
+
 app.use(helmet())
 app.use(morgan('combined'))
-
 app.use(cors())
 
 app.use((req, res, next) => {
@@ -31,26 +31,29 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'))
 
-app.use('/', expressProxy(`${config.api.host}:${config.api.port}`, {
+app.use('/', expressProxy(sawtoothRestApiUrl, {
   filter: (req, res) => {
-    return proxyRequest(req.path, config.api.endpoints)
+    return proxyRequest(req.path, env.sawtoothRestApi.endpoints)
   }
 }))
 
-  app.listen(config.proxy.securePort)
-  console.warn('Warning! Proxy is not secured via HTTPS!\n')
-} else {
-  app.listen(config.proxy.port)
 if (env.useHttps === false) {
+  app.listen(env.proxy.publicPort)
+  console.warn('Warning! Proxy is not secured via HTTPS!')
 
+  console.log(`Listening for requests at port ${env.proxy.publicPort}`)
+  console.log(`and forwarding them to http://${sawtoothRestApiUrl}`)
+
+} else {
+  app.listen(env.proxy.internalPort)
   https.createServer({
-    cert: fs.readFileSync(path.resolve(__dirname, config.proxy.sslCert)),
-    key: fs.readFileSync(path.resolve(__dirname, config.proxy.sslKey))
-  }, app).listen(config.proxy.securePort)
-}
+    cert: fs.readFileSync(path.resolve(__dirname, env.proxy.sslCert)),
+    key: fs.readFileSync(path.resolve(__dirname, env.proxy.sslKey))
+  }, app).listen(env.proxy.externalPort)
 
-console.log(`Listening for requests at port ${config.proxy.securePort} ...`)
-console.log(`... and forwarding them to ${config.api.host}:${config.api.port}`)
+  console.log(`Listening for requests at port ${env.proxy.publicPort}`)
+  console.log(`and forwarding them to https://${sawtoothRestApiUrl}`)
+}
 
 const proxyRequest = (path, endpoints) => {
   let match = false
